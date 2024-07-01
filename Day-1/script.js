afb0676c7be78b726fe7d3889e9b3626a43240dd
saakiyeahwith.c <prashant1.785401@cct.tu.edu.np>
2024-07-01 15:09:18 +0545

    first commit

---
+++ Day-1/script.js
@@ -0,0 +1,21 @@
+/*var bottle ="paani"//primitive data type
+let bottle = 'pani'
+const bottle = 'paani'*/
+
+/*const password = 'hello'
+
+
+
+const password = 'hahaha'*/
+const friend = ['Prashant', 21, 'Dharan', true] // console.log (friend[0]) friend variable bata 'prashant' print garauda 
+
+const name = 'Prahant'
+const age = 1
+const address = 'Dharan'
+
+const me = {
+    name: 'prashant',
+    age: 21,
+    address: 'Dharan'
+}
+console.log(friend[0])
