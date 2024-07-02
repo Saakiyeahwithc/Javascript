46baa4fc1462a95700fb9643daa5a8090ba1dfcb
saakiyeahwith.c <prashant1.785401@cct.tu.edu.np>
2024-07-02 14:42:53 +0545

    day-2

---
+++ Day-2/function.js
@@ -0,0 +1,74 @@
+//const weekdays = ['sunday', 'monday', 'tuesday', 'wednesday']
+//console.log(weekdays[2])
+//weekdays.push('thursday')
+//weekdays.pop()
+//weekdays.unshift('hi')
+//weekdays.shift()
+
+//const data = weekdays.slice(1,4)
+//console.log(weekdays)
+
+
+//weekdays.splice(1,1,'haha')
+
+//weekdays.splice(3,1,'byee')
+
+
+//const data=[1,2,3,4,5,6]
+
+//data.splice(1,3,'Hello','bye','World') //1=array position , 3 indicates array bata kati ota index change garne 
+//console.log(data)
+//const vowels = ['a', 'e', 'i', 'o', 'u']
+//vowels.splice(0, 5, 1, 2, 3, 4, 5)
+//console.log(vowels)
+
+//const data = ['p','r','a','s','h','a','n','t']
+//data.splice(0, 8, 16, 17, 1, 18, 8, 1, 14, 20)
+//console.log(data)
+
+
+/*const text = 'I  am from Nepal'
+const splitteddata = text.split(" ")
+console.log(splitteddata)*/
+
+
+//const lang = ['js', 'php', 'c']
+//lang[1]=html
+//lang.splice(0,1,'html')
+//console.log(lang)
+
+//freeze function lagayo vane stactic banaucha esle garda aru manche le data change garnu paudeina
+// const person = Object.freeze(
+// {
+//     name: "prashant",
+//     age: 21,
+//     address: "Dharan"
+// })
+// console.log(Object.keys(address))
+
+//condition and functions
+
+// const datas = {
+//     name: ["Prashant", "Shakya"]
+// }
+// datas.name.push('sanjeev')
+// console.log(datas)
+
+// const datas = [
+// {
+//     name : "prashant"
+// },
+// {
+//     name: "namxang"
+// }
+// ]
+//datas.name.push('Sanjeev')
+// datas.push({name:'sanjeev'})
+// datas.unshift({name:'sanjeev'})
+//console.log(datas)
+// const test2 = {
+//     name: ["Prashant", "Sanjeev", "namxang"]
+// }
+
+// test2.name.splice(0,3,1,2,3)
+// console.log(test2)
